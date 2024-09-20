92877cd55a9650d68848cef0925016a584beb7e1
ryryry-3302 <rjleong2002@gmail.com>
2024-09-20 17:37:04 +0800

    Use lambdas to filter task and sort

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -6,6 +6,8 @@ import se.edu.streamdemo.task.Task;
 
 import java.util.ArrayList;
 
+import static java.util.stream.Collectors.toList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Task manager (using streams)");
@@ -19,7 +21,13 @@ public class Main {
         printDeadlines(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
-
+        System.out.println("sorted");
+        printDeadlinesUsingStream(tasksData);
+        System.out.println("filter by stream");
+        ArrayList<Task> filteredlist = filterTaskByString(tasksData,"11");
+        for (Task task : filteredlist) {
+            System.out.println(task);
+        }
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -46,4 +54,16 @@ public class Main {
         }
     }
 
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasksData) {
+        tasksData.stream()
+                .filter((t) -> t instanceof Deadline)
+                .sorted((t1,t2)-> t1.getDescription().compareToIgnoreCase(t2.getDescription()))
+                .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filterTaskByString (ArrayList<Task> tasksData, String filterString) {
+        return (ArrayList<Task>) tasksData.stream()
+                                    .filter((t) -> t.getDescription().contains(filterString))
+                                    .collect(toList());
+    }
 }
